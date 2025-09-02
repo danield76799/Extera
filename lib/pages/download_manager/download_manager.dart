5a8814b3dddd948142b616eb2cc801c05888c935
OfficialDakari <squiggle_ground671@simplelogin.com>
2025-09-02 10:47:31 +0500

    boilerplate for background downloads

---
+++ lib/pages/download_manager/download_manager.dart
@@ -0,0 +1,16 @@
+import 'package:extera_next/generated/l10n/l10n.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class DownloadManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DownloadManagerController();
+}
+
+class DownloadManagerController extends State<DownloadManager> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+
+}
---
+++ lib/pages/download_manager/download_manager_view.dart
@@ -0,0 +1,12 @@
+import 'package:extera_next/pages/download_manager/download_manager.dart';
+import 'package:flutter/material.dart';
+
+class DownloadManagerView extends StatelessWidget {
+  final DownloadManagerController controller;
+  const DownloadManagerView(this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
